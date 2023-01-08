a4436e16e255c3828a83da0a1d385a78e9587283
theteachr <sop.theteachr@gmail.com>
2023-01-08 16:22:03 +0530

    Reformat with tabs

--- src/main.c
+++ src/main.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 struct Node {
-  void *data;
-  struct Node* next;
+	void *data;
+	struct Node* next;
 };
 
 struct GenLinkedList {
-  struct Node* head;
-  size_t data_size;
+	struct Node* head;
+	size_t data_size;
 };
 
 struct Node* get_node(void* data) {
@@ -21,12 +21,12 @@ struct Node* get_node(void* data) {
 }
 
 struct GenLinkedList* init_gen_linked_list(size_t data_size) {
-  struct GenLinkedList* list = malloc(sizeof(struct GenLinkedList));
+	struct GenLinkedList* list = malloc(sizeof(struct GenLinkedList));
 
-  list->head = get_node(NULL);
-  list->data_size = data_size;
+	list->head = get_node(NULL);
+	list->data_size = data_size;
 
-  return list;
+	return list;
 }
 
 void pop(struct GenLinkedList* list) {
@@ -76,30 +76,30 @@ void print_string(void* string) {
 }
 
 int main(void) {
-  struct GenLinkedList* numbers = init_gen_linked_list(sizeof(int));
-  struct GenLinkedList* names = init_gen_linked_list(sizeof(char*));
-  int nums[] = {1, 2, 3};
+	struct GenLinkedList* numbers = init_gen_linked_list(sizeof(int));
+	struct GenLinkedList* names = init_gen_linked_list(sizeof(char*));
+	int nums[] = {1, 2, 3};
 
-  char* viewers[] = {
-	  "daneelsan",
-	  "Zeeshan",
-	  "James",
-	  "Julestelliot",
-	  "Mohammad",
-  };
+	char* viewers[] = {
+		"daneelsan",
+		"Zeeshan",
+		"James",
+		"Julestelliot",
+		"Mohammad",
+	};
 
-  for (int i = 0; i < sizeof(viewers) / sizeof(char*); i++) {
-	  prepend(names, viewers[i]);
-  }
+	for (int i = 0; i < sizeof(viewers) / sizeof(char*); i++) {
+		prepend(names, viewers[i]);
+	}
 
-  for (int i = 0; i < sizeof(nums) / sizeof(int); i++) {
-	  prepend(numbers, &nums[i]);
-  }
+	for (int i = 0; i < sizeof(nums) / sizeof(int); i++) {
+		prepend(numbers, &nums[i]);
+	}
 
-  pop(numbers);
-  pop(names);
+	pop(numbers);
+	pop(names);
 
-  print_list(numbers, print_int);
-  print_list(names, print_string);
-  print_list(init_gen_linked_list(sizeof(int)), print_int);
+	print_list(numbers, print_int);
+	print_list(names, print_string);
+	print_list(init_gen_linked_list(sizeof(int)), print_int);
 }
